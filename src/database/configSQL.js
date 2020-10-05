bf8f59fa4ecce27b44d28cc6f71b8e15ac2763c8
Matheus Mendes Rezende <60859810+matheusm821@users.noreply.github.com>
2020-10-05 20:34:05 -0300

    ok

--- src/database/configSQL.js
+++ src/database/configSQL.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'bootwhatsapp.cwdkivtj8ka1.sa-east-1.rds.amazonaws.com',
     port: '3306',
     user: 'dev',
-    password: 'secret',
-    database: 'bootwhatsapp'
+    password: 'secret(!#)123TECH',
+    database: 'BurgerHouse'
 })
 
 exports.connection = connection
