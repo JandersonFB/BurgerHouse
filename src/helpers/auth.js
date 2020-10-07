e7bc37d5da19455935503b311e38b9e7ce761c3f
Matheus Mendes Rezende <60859810+matheusm821@users.noreply.github.com>
2020-10-07 20:54:49 -0300

    ok

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,6 +1,7 @@
 module.exports = {
     auth: function(req, res, next) {
         if (req.isAuthenticated()) {
+            req.session.save()
             return next()
         } else {
             req.flash('error_msg', 'Necessário realizar o login para acessar a pagina solicitada!')
