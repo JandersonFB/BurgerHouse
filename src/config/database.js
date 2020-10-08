bbe7f9f21fb25fd7eb5ca06c193eae32c1065990
Matheus Mendes Rezende <60859810+matheusm821@users.noreply.github.com>
2020-10-07 22:52:55 -0300

    ok

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports = {
     dialect: 'mysql',
-    host: process.env.HOST,
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: 'bootwhatsapp.cwdkivtj8ka1.sa-east-1.rds.amazonaws.com',
+    username: 'dev',
+    password: 'secret(!#)123TECH',
+    database: 'BurgerHouse',
 
 }
--- src/database/configSQL.js
+++ src/database/configSQL.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
-require('dotenv').config()
+
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: process.env.PORTA,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: 'bootwhatsapp.cwdkivtj8ka1.sa-east-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'dev',
+    password: 'secret(!#)123TECH',
+    database: 'BurgerHouse'
 })
 
 exports.connection = connection
