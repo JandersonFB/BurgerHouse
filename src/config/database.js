8ac7d9e6735a85ff026bf8a14b5e8f74f77004b0
Matheus Mendes Rezende <60859810+matheusm821@users.noreply.github.com>
2020-10-07 22:40:34 -0300

    ok

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
     dialect: 'mysql',
-    host: 'bootwhatsapp.cwdkivtj8ka1.sa-east-1.rds.amazonaws.com',
-    username: 'dev',
-    password: 'secret(!#)123TECH',
-    database: 'BurgerHouse',
+    host: process.env.HOST,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 
 }
-  /*  
-module.exports = {
-    dialect: 'mysql',
-    host: 'db4free.net',
-    username: 'matheusmlol',
-    password: '12052000a',
-    database: 'bootwhatsapp'
-}*/
